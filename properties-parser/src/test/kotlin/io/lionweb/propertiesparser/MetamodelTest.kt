6423154b7b5cfdbe4b59a5062da6e301af42d9d1
Federico Tomassetti <federico@strumenta.com>
2023-04-28 14:42:00 +0200

    adding test to load Properties metamodel

--- properties-parser/src/test/kotlin/io/lionweb/propertiesparser/MetamodelTest.kt
+++ properties-parser/src/test/kotlin/io/lionweb/propertiesparser/MetamodelTest.kt
@@ -1,6 +1,7 @@
 package io.lionweb.propertiesparser
 
 import com.google.gson.GsonBuilder
+import com.strumenta.kolasu.metamodel.StarLasuMetamodel
 import junit.framework.TestCase.assertEquals
 import org.junit.Test
 import org.lionweb.lioncore.java.serialization.JsonSerialization
@@ -11,5 +12,25 @@ class MetamodelTest {
     fun conceptFeaturesAreNotNull() {
         val propertiesFile = Metamodel.getConceptByName("PropertiesFile")!!
         assertEquals(false, propertiesFile.isAbstract)
+    }
+
+    @Test
+    fun propertiesMetamodelDependsOnStarLasu() {
+        assertEquals(listOf(StarLasuMetamodel), Metamodel.dependsOn())
+    }
+
+    @Test
+    fun loadingPropertyMetamodel() {
+        val propertiesMetamodelSerialization = JsonSerialization.getStandardSerialization()
+            .serializeTreeToJsonElement(Metamodel)
+        val starlasuMetamodelSerialization = JsonSerialization.getStandardSerialization()
+            .serializeTreeToJsonElement(StarLasuMetamodel)
+
+        val jsonSerialization = JsonSerialization.getStandardSerialization()
+        val starlasuUnserialized = jsonSerialization.unserializeToNode(starlasuMetamodelSerialization).first()
+        jsonSerialization.nodeResolver.addTree(starlasuUnserialized)
+        val propertiesUnserialized = jsonSerialization.unserializeToNode(propertiesMetamodelSerialization).first()
+        // We test we can load without crashing, as comparison of Metamodels with equality is not yet
+        // correctly implemented
     }
 }
