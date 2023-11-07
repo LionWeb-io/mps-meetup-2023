71bde05157d1f5aaa78daea057bf0a52b5d28ea7
Atanas <atanas@f1re.io>
2023-11-07 15:41:42 +0100

    Load explicit language model.

--- properties-parser/src/main/kotlin/io/lionweb/propertiesparser/PropertiesModelLoader.kt
+++ properties-parser/src/main/kotlin/io/lionweb/propertiesparser/PropertiesModelLoader.kt
@@ -1,6 +1,10 @@
 package io.lionweb.propertiesparser
 
-import io.lionweb.Properties.PropertiesFile
+import com.strumenta.kolasu.language.KolasuLanguage
+import com.strumenta.kolasu.lionweb.LionWebModelConverter
+import com.strumenta.kolasu.lionweb.StarLasuLWLanguage
+import io.lionweb.Properties.*
+import io.lionweb.lioncore.java.language.Language
 import io.lionweb.lioncore.java.language.LionCoreBuiltins
 import io.lionweb.lioncore.java.self.LionCore
 import io.lionweb.lioncore.java.serialization.JsonSerialization
@@ -8,9 +12,34 @@ import java.io.File
 import java.io.FileInputStream
 
 class PropertiesModelLoader {
+    private var language: Language;
+    private var jsonSer: JsonSerialization;
+
+    init {
+        var languageFile = File(".\\..\\..\\properties-parser\\properties.lmm.json")
+        require(languageFile.exists())
+        jsonSer = JsonSerialization.getStandardSerialization()
+        jsonSer.instanceResolver.addTree(StarLasuLWLanguage)
+        language = jsonSer.deserializeToNodes(FileInputStream(languageFile)).first() as Language
+        jsonSer.classifierResolver.registerLanguage(language)
+    }
+
     fun loadModel(file: File): PropertiesFile {
-        val jsonSer = JsonSerialization.getStandardSerialization()
-        jsonSer.registerLanguage(PropertiesLWLanguage)
-        return jsonSer.deserializeToNodes(FileInputStream(file))[0] as PropertiesFile
+        jsonSer.enableDynamicNodes()
+        val model = jsonSer.deserializeToNodes(FileInputStream(file))
+
+        val lwImpExp = LionWebModelConverter()
+        val kolasuLanguage = KolasuLanguage("properties").apply {
+            addClass(PropertiesFile::class)
+            addClass(Property::class)
+            addClass(Value::class)
+            addClass(IntValue::class)
+            addClass(BooleanValue::class)
+            addClass(StringValue::class)
+            addClass(DecValue::class)
+        }
+
+        lwImpExp.associateLanguages(language, kolasuLanguage)
+        return lwImpExp.importModelFromLionWeb(model.first()) as PropertiesFile
     }
 }
